ce853182806467c832cbbcb450d600cc7c76a015
Snewkis <Snewkis@yandex.com>
2019-02-05 19:00:35 +0100

    open + save + toolbar + download

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,50 @@ new p5();
 const WIDTH = 1200, HEIGHT = 800;
 
 let canvas;
+let img;
+
+$('#openImageBtn').change(() => {
+	const file = document.getElementById('openImageBtn').files[0];
+	const reader = new FileReader();
+
+	reader.onloadend = () => {
+		img = loadImage(reader.result, () => {
+			resizeCanvas(img.width, img.height);
+		});
+  	}
+
+  	if (file) {
+    	reader.readAsDataURL(file);
+  	}
+});
+
+$('#saveToolBarBtn').click(() => {
+	if (img == null) {
+		alert('No image');
+		return;
+	}
+
+	let ext = prompt('File extension (jpg, png) :')
+
+	if (ext) {
+		saveCanvas(canvas, 'Fotoko', ext);
+	}
+});
 
 function setup() {
 	canvas = createCanvas(WIDTH, HEIGHT);
+	canvas.id('canvas');
 	canvas.parent('#canvasParent');
 }
 
 function draw() {
 	background(200);
 
-	textAlign(CENTER);
-	textSize(50);
-	text('No image', width / 2, height / 2);
+	if (img == null) {
+		textAlign(CENTER);
+		textSize(50);
+		text('No image', width / 2, height / 2);
+	} else {
+		image(img, 0, 0);
+	}
 }
